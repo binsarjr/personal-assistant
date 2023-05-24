b6424db5b5d6b78ac92648f5acab3bc939561226
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-24 22:21:19 +0700

    dhlah

--- src/Handler/AutoDeleteWaSettingBug.ts
+++ src/Handler/AutoDeleteWaSettingBug.ts
@@ -1,25 +1,33 @@
 import { MessageUpsertType, proto } from "@whiskeysockets/baileys"
 import { HandlerArgs } from "../Contracts/IEventListener"
 import { MessageUpsert } from "../Facades/Events/Message/MessageUpsert"
-import Queue from "../Facades/Queue"
 
 export class AutoDeleteWaSettingBug extends MessageUpsert {
     patterns: RegExp = /wa\.me\/settings/ig
     fromMe: boolean = true
     async handler({ socket, props }: HandlerArgs<{ message: proto.IWebMessageInfo; type: MessageUpsertType }>): Promise<void> {
         const jid = props.message.key.remoteJid || ''
-        Queue(() => socket.sendMessage(jid, {
-            delete: props.message.key
-        }))
-
-        const message: { id: string, fromMe: boolean, timestamp: any } = {
-            id: props.message.key.id!,
-            fromMe: props.message.key.fromMe!,
-            timestamp: props.message.messageTimestamp
+        try {
+            await socket.sendMessage(jid, {
+                delete: props.message.key
+            })
+        } catch (error) {
 
         }
-        Queue(() => socket.chatModify({ clear: { messages: [message] } },
-            jid))
+
+        try {
+            const message: { id: string, fromMe: boolean, timestamp: any } = {
+                id: props.message.key.id!,
+                fromMe: props.message.key.fromMe!,
+                timestamp: props.message.messageTimestamp
+
+            }
+            await socket.chatModify({ clear: { messages: [message] } },
+                jid)
+        } catch (error) {
+
+        }
+
     }
 
 }
