7448bf463c53bda49af9ad38152632d9bac1e1f0
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-06-15 23:28:24 +0700

    add mediasaver tiktok downloader

--- src/Handler/Command/Downloader/TiktokDownloader.ts
+++ src/Handler/Command/Downloader/TiktokDownloader.ts
@@ -1,32 +1,33 @@
-import { tiktokdl } from '@bochilteam/scraper'
 import { MessageUpsertType, proto } from '@whiskeysockets/baileys'
 import { HandlerArgs } from '../../../Contracts/IEventListener'
 import { MessageUpsert } from '../../../Facades/Events/Message/MessageUpsert'
 import Queue from '../../../Facades/Queue'
 import { getMessageCaption, sendMessageWTyping } from '../../../utils'
 
-const download = async (link: string): Promise<string> => {
+const download = async (link: string): Promise<{video:string;images:string[]}> => {
   // @ts-ignore
-  const res = await Promise.any([
-    (async () => {
-      const res = await tiktokdl(link)
-      console.log(res, 'v1')
-      return (
-        res.video.no_watermark_hd
-      )
-    })(),
-    // (async () => {
-    //   const res = await tiktokdlv2(link)
-    //   console.log(res, 'v2')
-    //   return res.video.no_watermark_hd || res.video.no_watermark
-    // })(),
-    // (async () => {
-    //   const res = await tiktokdlv3(link)
-    //   console.log(res, 'v3')
-    //   return res.video.no_watermark2 || res.video.no_watermark
-    // })(),
-  ])
-  return res
+  // const res = await Promise.any([
+  //   (async () => {
+  //     const res = await tiktokdl(link)
+  //     console.log(res, 'v1')
+  //     return (
+  //       res.video.no_watermark_hd
+  //     )
+  //   })(),
+  // (async () => {
+  //   const res = await tiktokdlv2(link)
+  //   console.log(res, 'v2')
+  //   return res.video.no_watermark_hd || res.video.no_watermark
+  // })(),
+  // (async () => {
+  //   const res = await tiktokdlv3(link)
+  //   console.log(res, 'v3')
+  //   return res.video.no_watermark2 || res.video.no_watermark
+  // })(),
+  // ])
+  const resp = await fetch('https://mediasaver.vercel.app/services/tiktok/snaptik?url=' + link)
+  const json: { video: string,images:string[] } = await resp.json()
+  return json
 }
 
 export class TiktokDownloader extends MessageUpsert {
@@ -48,24 +49,35 @@ export class TiktokDownloader extends MessageUpsert {
     text.split(/\s+/).map((url) => {
       try {
         urls.push(new URL(url))
-      } catch (error) {}
+      } catch (error) { }
     })
     const jid = props.message.key.remoteJid!
     urls.map(async (url) => {
       try {
-        const videosrc = await download(url.toString())
-        Queue(() =>
+        const {video,images} = await download(url.toString())
+        await Queue(() =>
           sendMessageWTyping(
             {
               video: {
-                url: videosrc,
+                url: video,
               },
             },
             jid,
             socket,
           ),
         )
-      } catch (error) {}
+        images.map(image => Queue(() =>
+          sendMessageWTyping(
+            {
+              image: {
+                url: image,
+              },
+            },
+            jid,
+            socket,
+          ),
+        ))
+      } catch (error) { }
     })
   }
 }
