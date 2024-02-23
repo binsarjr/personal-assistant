1a75d06bcf3d72bdbcb5dc6831b9ee71da175621
binsarjr <binsarjr121@gmail.com>
2024-02-24 01:58:53 +0700

    view once message reveal

---
+++ src/actions/message/alwaysExecuted/AutoViewOnceRevealAction.ts
@@ -0,0 +1,77 @@
+import {
+	downloadMediaMessage,
+	type WAMessage,
+	type WASocket,
+} from "@whiskeysockets/baileys";
+import BaseMessageHandlerAction from "../../../foundation/actions/BaseMessageHandlerAction.js";
+import { QueueMessage } from "../../../services/queue.js";
+import { getJid, react, sendWithTyping } from "../../../supports/message.js";
+import type { MessagePattern } from "../../../types/MessagePattern.js";
+
+export default class extends BaseMessageHandlerAction {
+	patterns(): MessagePattern {
+		return true;
+	}
+
+	async isEligibleToProcess(
+		socket: WASocket,
+		message: WAMessage
+	): Promise<boolean> {
+		return !message.key.fromMe;
+	}
+
+	async process(socket: WASocket, message: WAMessage): Promise<void> {
+		const viewOnceMessage =
+			message.message?.viewOnceMessage ||
+			message.message?.viewOnceMessageV2 ||
+			message.message?.viewOnceMessageV2Extension ||
+			message;
+
+		const isViewOnce =
+			viewOnceMessage?.message?.imageMessage?.viewOnce ||
+			viewOnceMessage?.message?.videoMessage?.viewOnce;
+
+		if (isViewOnce) {
+			const image = viewOnceMessage?.message?.imageMessage;
+			const video = viewOnceMessage?.message?.videoMessage;
+			const caption = image?.caption || video?.caption;
+			react(socket, "👀", message);
+			let text = `View Once Message Revealed\n`;
+			if (caption) {
+				text += `Caption: ${caption}\n`;
+			}
+
+			const media = await downloadMediaMessage(message, "buffer", {});
+
+			text = text.trim();
+			const jid = getJid(message);
+			if (image) {
+				QueueMessage.add(() =>
+					sendWithTyping(
+						socket,
+						{
+							image: media as Buffer,
+							caption: text,
+						},
+						jid,
+
+						{ quoted: message }
+					)
+				);
+			} else if (video) {
+				QueueMessage.add(() =>
+					sendWithTyping(
+						socket,
+						{
+							video: media as Buffer,
+							caption: text,
+						},
+						jid,
+
+						{ quoted: message }
+					)
+				);
+			}
+		}
+	}
+}
--- src/configs/command.ts
+++ src/configs/command.ts
@@ -1,6 +1,7 @@
+import AutoViewOnceRevealAction from "../actions/message/alwaysExecuted/AutoViewOnceRevealAction.js";
 import PingAction from "../actions/message/random/PingAction.js";
 import type CommandConfig from "../types/CommandConfig.js";
 
 export const commands: CommandConfig = {
-	messagesHandler: [new PingAction()],
+	messagesHandler: [new PingAction(), new AutoViewOnceRevealAction()],
 };
