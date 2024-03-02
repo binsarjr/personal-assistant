e477ab8fff22b7f307c4e6cad161e38f5f3a2190
binsarjr <binsarjr121@gmail.com>
2024-03-02 21:55:47 +0700

    update auto revela view once

--- src/actions/message/alwaysExecuted/AutoViewOnceRevealAction.ts
+++ src/actions/message/alwaysExecuted/AutoViewOnceRevealAction.ts
@@ -1,77 +1,70 @@
 import {
-	downloadMediaMessage,
-	jidNormalizedUser,
-	type WAMessage,
-	type WASocket,
+  downloadMediaMessage,
+  jidNormalizedUser,
+  type AnyMessageContent,
+  type WAMessage,
+  type WASocket,
 } from "@whiskeysockets/baileys";
 import BaseMessageHandlerAction from "../../../foundation/actions/BaseMessageHandlerAction.js";
 import { QueueMessage } from "../../../services/queue.js";
-import { react, sendWithTyping } from "../../../supports/message.js";
+import { sendWithTyping } from "../../../supports/message.js";
 import type { MessagePattern } from "../../../types/MessagePattern.js";
 
 export default class extends BaseMessageHandlerAction {
-	patterns(): MessagePattern {
-		return true;
-	}
+  patterns(): MessagePattern {
+    return true;
+  }
 
-	async isEligibleToProcess(
-		socket: WASocket,
-		message: WAMessage
-	): Promise<boolean> {
-		return !message.key.fromMe;
-	}
+  async isEligibleToProcess(
+    socket: WASocket,
+    message: WAMessage,
+  ): Promise<boolean> {
+    return !message.key.fromMe;
+  }
 
-	async process(socket: WASocket, message: WAMessage): Promise<void> {
-		const viewOnceMessage =
-			message.message?.viewOnceMessage ||
-			message.message?.viewOnceMessageV2 ||
-			message.message?.viewOnceMessageV2Extension ||
-			message;
+  async process(socket: WASocket, message: WAMessage): Promise<void> {
+    const viewOnceMessage =
+      message.message?.viewOnceMessage ||
+      message.message?.viewOnceMessageV2 ||
+      message.message?.viewOnceMessageV2Extension ||
+      message;
 
-		const isViewOnce =
-			viewOnceMessage?.message?.imageMessage?.viewOnce ||
-			viewOnceMessage?.message?.videoMessage?.viewOnce;
+    const isViewOnce =
+      viewOnceMessage?.message?.imageMessage?.viewOnce ||
+      viewOnceMessage?.message?.videoMessage?.viewOnce;
 
-		if (isViewOnce) {
-			const image = viewOnceMessage?.message?.imageMessage;
-			const video = viewOnceMessage?.message?.videoMessage;
-			const caption = image?.caption || video?.caption;
-			react(socket, "👀", message);
-			let text = `View Once Message Revealed\n`;
-			if (caption) {
-				text += `Caption: ${caption}\n`;
-			}
+    if (isViewOnce) {
+      const image = viewOnceMessage?.message?.imageMessage;
+      const video = viewOnceMessage?.message?.videoMessage;
+      const caption = image?.caption || video?.caption;
 
-			const media = await downloadMediaMessage(message, "buffer", {});
+      const media = await downloadMediaMessage(message, "buffer", {});
 
-			text = text.trim();
-			if (image) {
-				QueueMessage.add(() =>
-					sendWithTyping(
-						socket,
-						{
-							image: media as Buffer,
-							caption: text,
-						},
-						jidNormalizedUser(socket.user?.id!),
+      let content: AnyMessageContent | null = null;
 
-						{ quoted: message }
-					)
-				);
-			} else if (video) {
-				QueueMessage.add(() =>
-					sendWithTyping(
-						socket,
-						{
-							video: media as Buffer,
-							caption: text,
-						},
-						jidNormalizedUser(socket.user?.id!),
+      if (image) {
+        content = {
+          image: media as Buffer,
+        };
+      } else if (video) {
+        content = {
+          video: media as Buffer,
+        };
+      }
 
-						{ quoted: message }
-					)
-				);
-			}
-		}
-	}
+      if (content && caption) {
+        content.caption = caption;
+      }
+
+      if (content)
+        QueueMessage.add(() =>
+          sendWithTyping(
+            socket,
+            content!,
+            jidNormalizedUser(socket.user?.id!),
+            { quoted: message },
+          ),
+        );
+    }
+  }
 }
