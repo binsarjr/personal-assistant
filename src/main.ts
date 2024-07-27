8ab634133e7f633810e99f96e3e2a58d81315f22
binsarjr <binsarjr121@gmail.com>
2024-07-28 00:05:08 +0700

    refactor: create device and login automation when devices is already

--- src/main.ts
+++ src/main.ts
@@ -4,30 +4,32 @@ import { WhatsappConnectionService } from '@app/whatsapp/core/whatsapp-connectio
 import { WhatsappModule } from '@app/whatsapp/whatsapp.module';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaModule, PrismaService } from '@app/prisma';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // await app.listen(3000);
-  // await CommandFactory.run(AppModule);
-
-  if (process.argv.includes('--login')) {
-    const loginApp = await NestFactory.createApplicationContext(AppModule);
-
-    const service = loginApp.select(WhatsappActionModule).get(ScanQrCodeAction);
-    await service.scan('personal-asistant');
-
-    await loginApp.close();
-    return;
-  }
-
   const app = await NestFactory.createApplicationContext(AppModule);
 
-  const whatsappConnection = app
-    .select(WhatsappModule.forRoot())
-    .get(WhatsappConnectionService);
-  whatsappConnection.connectingAllDevice();
+  const prisma = app.select(PrismaModule.forRoot()).get(PrismaService);
+  await prisma.$connect();
+
+  const devices = await prisma.device.findMany();
+  if (devices.length === 0) {
+    const service = app.select(WhatsappActionModule).get(ScanQrCodeAction);
+    await service.scan('personal-asistant');
+  } else {
+    const whatsappConnection = app
+      .select(WhatsappModule.forRoot())
+      .get(WhatsappConnectionService);
+    await whatsappConnection.connectingAllDevice();
+  }
 
   await app.close();
 }
 
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log('Bootstrap done');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
