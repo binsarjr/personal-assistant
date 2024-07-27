65d604729a906b5461e60e7cb8b5aa2b1eccc37f
binsarjr <binsarjr121@gmail.com>
2024-07-28 00:04:16 +0700

    fix: wrong apply method decorator

--- libs/whatsapp/src/core/whatsapp-event.service.ts
+++ libs/whatsapp/src/core/whatsapp-event.service.ts
@@ -1,5 +1,8 @@
 import { WhatsappEventActionMetadataKey } from '@app/whatsapp/constants';
-import { DiscoveryService } from '@golevelup/nestjs-discovery';
+import {
+  DiscoveredMethodWithMeta,
+  DiscoveryService,
+} from '@golevelup/nestjs-discovery';
 import { Injectable } from '@nestjs/common';
 
 import type { BaileysEventMap, WASocket } from '@whiskeysockets/baileys';
@@ -14,19 +17,26 @@ export class WhatsappEventService {
     );
 
     const handlers: Partial<{
-      [key in keyof BaileysEventMap]: any[];
+      [key in keyof BaileysEventMap]: DiscoveredMethodWithMeta<any>[];
     }> = {};
 
     providers.map(async (provider) => {
       const eventName = provider.meta as unknown as keyof BaileysEventMap;
       if (!handlers[eventName]) handlers[eventName] = [];
-      handlers[eventName].push(provider.discoveredMethod.handler);
+      handlers[eventName].push(provider);
     });
 
     for (const eventName in handlers) {
       if (!handlers[eventName]) continue;
       socket.ev.on(eventName as keyof BaileysEventMap, (...args) => {
-        handlers[eventName].map((handler) => handler(socket, ...args));
+        handlers[eventName].map((handler: DiscoveredMethodWithMeta<any>) => {
+          const classInstance = handler.discoveredMethod.parentClass.instance;
+
+          handler.discoveredMethod.handler.apply(classInstance, [
+            socket,
+            ...args,
+          ]);
+        });
       });
     }
   }
