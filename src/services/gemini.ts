93edc1bbca9297b48abed45e79a35a4d2e9be02d
binsarjr <binsarjr121@gmail.com>
2024-08-08 00:16:48 +0700

    feat: safety setting

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -2,6 +2,8 @@ import {
   Content,
   GenerativeModel,
   GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
 } from '@google/generative-ai';
 
 export type GenerativeModelName =
@@ -27,6 +29,24 @@ export class Gemini {
 
     this.model = this.gemini.getGenerativeModel({
       model: model,
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.BLOCK_NONE,
+        },
+      ],
     });
     return this;
   }
