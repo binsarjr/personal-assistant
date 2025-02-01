b6da4b91e35ce921e576ebc82b809af18e98e8d2
binsarjr <binsarjr121@gmail.com>
2025-02-02 03:13:10 +0700

    refactor: improve shell command handler with better error and output handling

--- src/modules/owner/shell.handler.ts
+++ src/modules/owner/shell.handler.ts
@@ -1,32 +1,34 @@
-import { Command, Socket } from "$core/decorators";
-import { CommandMiddleware } from "$core/decorators/command-middleware";
-import { IsOnlyMeMiddleware } from "$infrastructure/whatsapp/middlewares/is-me.middleware";
-import type { SocketClient } from "$infrastructure/whatsapp/types";
-import { getMessageCaption } from "$support/whatsapp.support";
-import type { WAMessage } from "@whiskeysockets/baileys";
-import { $ } from "bun";
+import { Command, Socket } from '$core/decorators';
+import { CommandMiddleware } from '$core/decorators/command-middleware';
+import { IsOnlyMeMiddleware } from '$infrastructure/whatsapp/middlewares/is-me.middleware';
+import type { SocketClient } from '$infrastructure/whatsapp/types';
+import { getMessageCaption } from '$support/whatsapp.support';
+import type { WAMessage } from '@whiskeysockets/baileys';
+import { $ } from 'bun';
 
 export class ShellHandler {
-	@Command(/^\$.*/)
-	@CommandMiddleware(IsOnlyMeMiddleware)
-	async shell(@Socket() socket: SocketClient, message: WAMessage) {
-		const caption = getMessageCaption(message.message!).replace(
-			/^\$(\s+)?/,
-			""
-		);
+  @Command(/^\$.*/)
+  @CommandMiddleware(IsOnlyMeMiddleware)
+  async shell(@Socket() socket: SocketClient, message: WAMessage) {
+    const caption = getMessageCaption(message.message!).replace(
+      /^\$(\s+)?/,
+      '',
+    );
 
-		const { stdout, stderr } = await $`${{ raw: caption }}`;
+    const { stdout, stderr } = await $`${{ raw: caption }}`;
+    const stderrStr = stderr ? stderr.toString().trim() : '';
+    const stdoutStr = stdout ? stdout.toString().trim() : '';
 
-		if (stderr) {
-			await socket.replyQuote({
-				text: stderr.toString().trim(),
-			});
-		}
+    if (stderrStr) {
+      await socket.replyQuote({
+        text: stderrStr,
+      });
+    }
 
-		if (stdout) {
-			await socket.replyQuote({
-				text: stdout.toString().trim(),
-			});
-		}
-	}
+    if (stdoutStr) {
+      await socket.replyQuote({
+        text: stdoutStr,
+      });
+    }
+  }
 }
