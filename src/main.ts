ec8a5370ed5d90a1a48c52e9dabd88321358cf06
binsarjr <binsarjr121@gmail.com>
2025-02-16 06:35:46 +0700

    refactor: update anti-deleted message handler to use in-memory message store

--- src/main.ts
+++ src/main.ts
@@ -50,17 +50,17 @@ await whatsapp.initialize();
 logger.info('WhatsApp client initialized 🚀');
 
 {
-  // sementara
-  setInterval(async () => {
-    const count = await prisma.whatsappMessage.count();
-    const res = await prisma.whatsappMessage.deleteMany({
-      where: {
-        messageTimeUtc: {
-          lt: new Date(Date.now() - 1000 * 60 * 60 * 24),
-        },
-      },
-    });
-    logger.info(`total saved message: ${count}`);
-    logger.info(`deleted saved message: ${res.count}`);
-  }, 60 * 1_000);
+  // // sementara
+  // setInterval(async () => {
+  //   const count = await prisma.whatsappMessage.count();
+  //   const res = await prisma.whatsappMessage.deleteMany({
+  //     where: {
+  //       messageTimeUtc: {
+  //         lt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+  //       },
+  //     },
+  //   });
+  //   logger.info(`total saved message: ${count}`);
+  //   logger.info(`deleted saved message: ${res.count}`);
+  // }, 60 * 1_000);
 }
