be1b60f5ba6e1cdbc298803fadabd74471b259e8
binsarjr <binsarjr121@gmail.com>
2025-02-17 02:08:22 +0700

    refactor: update auto-reveal delete message handler to use messages.update event

--- src/modules/always-executed/auto-reveal-delete-message.handler.ts
+++ src/modules/always-executed/auto-reveal-delete-message.handler.ts
@@ -1,5 +1,4 @@
 import { ReadMoreUnicode } from '$infrastructure/config/consts.config';
-import { logger } from '$infrastructure/logger/console.logger';
 import wa_store from '$infrastructure/whatsapp/whatsapp-store';
 import {
   downloadContentBufferFromMessage,
@@ -11,95 +10,94 @@ import {
   isJidStatusBroadcast,
   jidDecode,
   jidNormalizedUser,
+  proto,
+  type BaileysEventMap,
   type WAMessage,
 } from '@whiskeysockets/baileys';
-import { Context, OnText, Socket, type SocketClient } from 'baileys-decorators';
+import {
+  Context,
+  OnEvent,
+  Socket,
+  type SocketClient,
+} from 'baileys-decorators';
 
 export class AntiDeletedMessageHandler {
-  @OnText(/.*/)
-  async execute(@Socket socket: SocketClient, @Context message: WAMessage) {
-    if (message.key.fromMe) {
-      return;
-    }
-    if (!message.message) {
-      logger.info('message.message is null from AntiDeletedMessageAction');
-      return;
-    }
-    const protocol = JSON.parse(JSON.stringify(message.message)) as {
-      protocolMessage: {
-        key: {
-          remoteJid: string;
-          fromMe: boolean;
-          id: string;
-        };
-        type: 'REVOKE';
-      };
-    };
-
-    if (protocol?.protocolMessage?.type !== 'REVOKE') {
-      return;
-    }
-
-    const deleted = wa_store.getDeletedMessage(
-      jidNormalizedUser(message.key.remoteJid!),
-      message.message?.protocolMessage?.key?.id!,
-    );
-    if (deleted) {
-      const message = deleted;
-      const jid = message.key.remoteJid!;
-      const type = getContentType(message.message!);
-
-      // tidak butuh karena sudah ada fitur anti view once message
-      if (
-        type === 'viewOnceMessage' ||
-        type === 'viewOnceMessageV2' ||
-        type === 'viewOnceMessageV2Extension'
-      ) {
-        return;
+  @OnEvent('messages.update')
+  async execute(
+    @Socket socket: SocketClient,
+    @Context updates: BaileysEventMap['messages.update'],
+  ) {
+    for (const { update, key } of updates) {
+      if (key.fromMe) {
+        continue;
       }
 
-      const response = [];
-
-      if (isJidStatusBroadcast(jid)) {
-        response.push('Story Whatsapp');
-      } else if (isJidGroup(jid)) {
-        const metadata = await socket.groupMetadata(jid);
-        response.push('Grup: *' + metadata.subject + '*');
+      if (update.messageStubType != proto.WebMessageInfo.StubType.REVOKE) {
+        continue;
       }
 
-      response.push(
-        'User *' + (message?.verifiedBizName || message?.pushName) + '*',
+      const deleted = wa_store.getDeletedMessage(
+        jidNormalizedUser(key.remoteJid!),
+        key?.id!,
       );
+      if (deleted) {
+        const message = deleted;
+        const jid = message.key.remoteJid!;
+        const type = getContentType(message.message!);
 
-      response.push(
-        'NoHP: ' +
-          jidDecode(message.key.participant || message.key.remoteJid!)?.user,
-      );
+        // tidak butuh karena sudah ada fitur anti view once message
+        if (
+          type === 'viewOnceMessage' ||
+          type === 'viewOnceMessageV2' ||
+          type === 'viewOnceMessageV2Extension'
+        ) {
+          continue;
+        }
 
-      const formatterDate = new Intl.DateTimeFormat('id', {
-        dateStyle: 'full',
-        timeStyle: 'long',
-      });
+        const response = [];
 
-      response.push(ReadMoreUnicode);
-      response.push(
-        'Waktu Dibuat: ' +
-          formatterDate.format(new Date(+message.messageTimestamp! * 1000)),
-      );
-      response.push('Waktu Dihapus: ' + formatterDate.format(new Date()));
+        if (isJidStatusBroadcast(jid)) {
+          response.push('Story Whatsapp');
+        } else if (isJidGroup(jid)) {
+          const metadata = await socket.groupMetadata(jid);
+          response.push('Grup: *' + metadata.subject + '*');
+        }
 
-      const sended = await this.resolveDeletedMessage(socket, message);
-      if (sended) {
-        await socket.sendMessage(
-          socket.user!.id,
-          {
-            text: response.join('\n\n'),
-          },
-          { quoted: sended },
+        response.push(
+          'User *' + (message?.verifiedBizName || message?.pushName) + '*',
         );
+
+        response.push(
+          'NoHP: ' +
+            jidDecode(message.key.participant || message.key.remoteJid!)?.user,
+        );
+
+        const formatterDate = new Intl.DateTimeFormat('id', {
+          dateStyle: 'full',
+          timeStyle: 'long',
+        });
+
+        response.push(ReadMoreUnicode);
+        response.push(
+          'Waktu Dibuat: ' +
+            formatterDate.format(new Date(+message.messageTimestamp! * 1000)),
+        );
+        response.push('Waktu Dihapus: ' + formatterDate.format(new Date()));
+
+        const sended = await this.resolveDeletedMessage(socket, message);
+        if (sended) {
+          await socket.sendMessage(
+            socket.user!.id,
+            {
+              text: response.join('\n\n'),
+            },
+            { quoted: sended },
+          );
+        }
       }
     }
   }
+
   async resolveDeletedMessage(
     socket: SocketClient,
     message: WAMessage,
